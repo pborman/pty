e17e5ed90cd195a3f08b024cb0b3645ea70fd1c5
Paul Borman <paul@borman.com>
2023-01-22 16:02:45 -0600

    Greatly improve the debugging ability of the mutex package.
    Add a Dump routine that can dump out the current state of all muticies.

--- mutex/mutex.go
+++ mutex/mutex.go
@@ -12,48 +12,187 @@
 //   See the License for the specific language governing permissions and
 //   limitations under the License.
 
+// Package mutex provides a mutex lock that can be traced when debugging.
+// Internally the mutex contains a sync.Mutex to handle the actual locking,
+// blocking, and releasing. Every Mutex must be initialized with New and
+// provided a name.  Lock is provided with who wants to lock the lock.  Since
+// the correspoding unlock *must* use the sane name provided to Lock, the method
+// to unlock the lock is returned.
+//
+// Examples
+//
+//	m := mutex.New("my lock")
+//
+//	unlock := m.Lock("fred")
+//	...
+//	unlock()
+//
+//	// Defer the unlock routine
+//	defer m.Lock("bob")()
+//
+// If the environment variable __MUTEX_DEBUG has the value of "true" when the
+// first lock is initialized, debugging and tracing is turned on.  When
+// __MUTEX_DEBUG is true then the Dump routine will print out a list of all held
+// locks, who is holding it, and who is blocked on it.  Debugging is heavy
+// weight.  Each time a lock is taken an internal sync.Mutex is locked and
+// unlocked twice.  When the lock is released the internal sync.Mutex is also
+// locked and unlocked.  The function runtime.Caller is also called for each
+// lock and unlock when debugging is enabled.
+//
+// When __MUTEX_DEBUG is not set, or set to "false" then debugging is not
+// enabled and this package turns into a thin wrapper around sync.Mutex.
+//
+// BUG:  Currently this package use the github.com/pborman/pty/log logging
+// package when debugging.  I would like to remove the dependencies however
+// there is a race condition during initialization between the first call to New
+// and when a logger could be registered.
 package mutex
 
 import (
 	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+	"runtime"
+	"strings"
 	"sync"
 
 	"github.com/pborman/pty/log"
 )
 
+// A Mutex is a mutex.
 type Mutex struct {
-	msg string
-	mu  sync.Mutex
+	name    string
+	mu      sync.Mutex
+	imu     sync.Mutex
+	owner   string
+	index   int
+	waiting map[string]struct{}
 }
 
-const debug = true
-
-type waiter *int
+var (
+	mu        sync.Mutex
+	list      []*Mutex // A list of all the mutexes ever created.
+	index     int      // to make all mutex names unique
+	underTest = func(string) {}
+	debug     = false
+	once      sync.Once
+	logger    = log.Outputf
+)
 
 // New returns a new named mutex.
-func New(msg string) *Mutex {
-	m := &Mutex{
-		msg: msg,
+func New(name string) *Mutex {
+	once.Do(func() {
+		// The very first time we are called we determine if we are
+		// debugging or not.
+		switch strings.ToLower(os.Getenv("__MUTEX_DEBUG")) {
+		case "t", "true", "yes", "1":
+			debug = true
+		}
+	})
+	if !debug {
+		return &Mutex{}
 	}
+	m := &Mutex{
+		name:    location(index, name),
+		waiting: map[string]struct{}{},
+	}
+	index++
+	mu.Lock()
+	list = append(list, m)
+	mu.Unlock()
 	return m
+}
+
+// Lock waits until it aquires the mutex log m and then returns the function
+// that will unlock m.
+func (m *Mutex) Lock(who string) func() {
+	// When debug is not set we are just a thin wrapper around sync.Mutex.
+	if !debug {
+		m.mu.Lock()
+		return m.mu.Unlock
+	}
+
+	who = location(-1, who)
+	m.logf("%s waiting for mutex", who)
+
+	// Mark us waiting on the lock
+	m.imu.Lock()
+	{
+		m.waiting[who] = struct{}{}
+		m.imu.Unlock()
+	}
+
+	m.mu.Lock()
+
+	// Mark us as owner of the lock and no longer waiting.
+	m.imu.Lock()
+	{
+		delete(m.waiting, who)
+		m.owner = who
+	}
+	m.imu.Unlock()
+	m.logf("%s acquired", who)
+
+	return func() {
+		m.logf("%s releasing mutex", who)
+		var owner string
+		m.imu.Lock()
+		{
+			owner = who
+			m.owner = ""
+		}
+		m.imu.Unlock()
+
+		if owner != who {
+			who := fmt.Sprintf("Mutex %s owned by %s and %s", m.name, owner, who)
+			if underTest != nil {
+				underTest(who)
+			} else {
+				panic(who)
+			}
+		}
+		m.mu.Unlock()
+	}
+}
+
+// Dump dumps the state of all non-idle muticies to w if the environment variable
+// __MUTEX_DEBUG is set to "true".  An environment variable is used as it is the
+// only reasonable way to turn debugging on prior to the first call to New.
+func Dump(w io.Writer) {
+	if !debug {
+		return
+	}
+	mu.Lock()
+	defer mu.Unlock()
+	for _, m := range list {
+		// Don't lock m, we are looking for mutecies that are currently
+		// locked with potentially waiting callers.
+		if m.owner != "" {
+			fmt.Fprintf(w, "mutex %s is locked by %s\n", m.name, m.owner)
+		} else if len(m.waiting) != 0 {
+			// This really should never happen.
+			fmt.Fprintf(w, "mutex %s is idle\n", m.name)
+		}
+		for name := range m.waiting {
+			fmt.Fprintf(w, "   %s waiting\n", name)
+		}
+	}
 }
 
 func (m *Mutex) logf(format string, args ...interface{}) {
 	if !debug {
 		return
 	}
-	format = fmt.Sprintf("%p:%s: %s", m, m.msg, format)
-	log.Outputf(3, "M", format, args...)
+	format = fmt.Sprintf("%s (%s)", format, m.name)
+	logger(3, "M", format, args...)
 }
 
-// Lock locks a mutex and returns the function to unlock the mutex.
-func (m *Mutex) Lock(msg string) func() {
-	m.logf("%s waiting for mutex\n", msg)
-	m.mu.Lock()
-	m.logf("%s acquired\n", msg)
-
-	return func() {
-		m.logf("%s releasing for mutex\n", msg)
-		m.mu.Unlock()
+func location(x int, msg string) string {
+	_, file, line, _ := runtime.Caller(2)
+	file = filepath.Base(file)
+	if x < 0 {
+		return fmt.Sprintf("<%s:%d %s>", file, line, msg)
 	}
+	return fmt.Sprintf("<[%d] %s:%d %s>", x, file, line, msg)
 }
--- mutex/mutex_test.go
+++ mutex/mutex_test.go
@@ -15,33 +15,279 @@
 package mutex
 
 import (
+	"bytes"
+	"fmt"
+	"os"
+	"strings"
 	"testing"
 	"time"
 )
 
-func TestSimple(t *testing.T) {
-	m := New("this is a mutext")
-	unlock := m.Lock("test lock")
-	m.imu.Lock()
-	if want, got := 0, len(m.waiters); want != got {
-		t.Errorf("Got %d waiters, want %d", want, got)
-	}
-	m.imu.Unlock()
-	c := make(chan struct{})
+func reset(db bool) {
+	list = nil
+	index = 0
+	debug = db
+	underTest = func(string) {}
+}
+
+func init() {
+	// Disable logging
+	logger = func(n int, p, format string, v ...interface{}) {}
+}
+
+func TestNoDebug(t *testing.T) {
+	reset(false)
+	m := New("D")
+
+	unlock := m.Lock("1")
+
+	here := false
 	go func() {
-		unlock := m.Lock("Blocked")
-		if want, got := 0, len(m.waiters); want != got {
-			t.Errorf("Got %d waiters, want %d", want, got)
-		}
-		unlock()
-		c <- struct{}{}
+		defer m.Lock("2")()
+		here = true
 	}()
 	time.Sleep(time.Second / 100)
-	m.imu.Lock()
-	if want, got := 1, len(m.waiters); want != got {
-		t.Errorf("Got %d waiters, want %d", want, got)
+	if here {
+		t.Errorf("double lock")
 	}
-	m.imu.Unlock()
 	unlock()
-	<-c
+
+	time.Sleep(time.Second / 100)
+	if !here {
+		t.Errorf("lock blocked")
+	}
+}
+func TestSimple(t *testing.T) {
+	reset(true)
+	m := New("M")
+	unlock := m.Lock("L")
+
+	if want, got := 0, len(m.waiting); want != got {
+		t.Errorf("Got %d waiting, want %d", want, got)
+	}
+
+	fdone := make(chan struct{})
+	go func() {
+		unlock := m.Lock("B")
+		t.Log("B took the lock")
+		if want, got := 0, len(m.waiting); want != got {
+			t.Errorf("Got %d waiting, want %d", want, got)
+		}
+		unlock()
+		close(fdone)
+	}()
+	time.Sleep(time.Second / 100)
+	tm := time.NewTimer(time.Second)
+
+	go func() {
+		<-tm.C
+		t.Errorf("timed out")
+		Dump(os.Stderr)
+	}()
+
+	if want, got := 1, len(m.waiting); want != got {
+		t.Errorf("Got %d waiting, want %d", want, got)
+	}
+	tm.Stop()
+	unlock()
+	<-fdone // wait for the goroutine to finish
+}
+
+func TestDump(t *testing.T) {
+	reset(true)
+	m1 := New("M1")
+	m2 := New("M2")
+	m3 := New("M3")
+	m3.waiting["ghost"] = struct{}{}
+
+	unlock1 := m1.Lock("B1")
+	unlock2 := m2.Lock("B1")
+
+	locked := make(chan bool)   // a goroutine got M1
+	unlockit := make(chan bool) // tell a goroutine to unlock M1
+	released := make(chan bool) // a goroutine released M1
+
+	// Start up 3 goroutines, all blocked on M1.
+	for i := 1; i <= 3; i++ {
+		g := fmt.Sprintf("G%d", i)
+		go func() {
+			unlock := m1.Lock(g)
+			locked <- true
+			// Main thread is doing a dump at this time.
+			<-unlockit
+			unlock()
+			released <- true
+		}()
+	}
+
+	// Wait for all three goroutines to block on M1
+	for {
+		time.Sleep(time.Second / 1000)
+		m1.imu.Lock()
+		n := len(m1.waiting)
+		m1.imu.Unlock()
+		if n == 3 {
+			break
+		}
+	}
+
+	// goUnlock tells one goroutine to unlock and waits for it to do so.
+	goUnlock := func() {
+		unlockit <- true
+		<-released
+	}
+
+	var lines []string
+	var failed bool
+
+	buf := &bytes.Buffer{}
+
+	takeDump := func() {
+		failed = false
+		buf.Reset()
+		Dump(buf)
+		s := buf.String()
+		if len(s) == 0 {
+			lines = []string{}
+			return
+		}
+		s = s[:len(s)-1] // trailing newline
+		lines = strings.Split(s, "\n")
+	}
+
+	lineHas := func(n int, s string) (ok, exists bool) {
+		if n >= len(lines) {
+			return false, false
+		}
+		return strings.Contains(lines[n], s), true
+	}
+
+	type test struct {
+		n    int
+		want string
+	}
+
+	testFunc := func(name string, tt test) {
+		ok, exists := lineHas(tt.n, tt.want)
+		if tt.want == "" {
+			if exists {
+				failed = true
+				t.Errorf("%s: Extra line %d:%s", name, tt.n, lines[tt.n])
+			}
+		} else if !exists {
+			failed = true
+			t.Errorf("%s Line %d missing", name, tt.n)
+		} else if !ok {
+			failed = true
+			t.Errorf("%s Line %d missing %s", name, tt.n, tt.want)
+		}
+	}
+
+	// M1 is locked
+	// G1, G2, G3 are blocked on M1
+	// M2 is locked
+	// M3 is not locked but "ghost" is waiting on it
+	// We also know that list is ordered based on the order we created the muticies.
+	takeDump()
+	for _, tt := range []test{
+		{n: 0, want: "M1> is locked by"},
+		{n: 1, want: "waiting"},
+		{n: 2, want: "waiting"},
+		{n: 3, want: "waiting"},
+		{n: 4, want: "M2> is locked by"},
+		{n: 5, want: "M3> is idle"},
+		{n: 6, want: "ghost waiting"},
+		{n: 8, want: ""},
+	} {
+		testFunc("Initial State", tt)
+	}
+
+	// Release M2
+	unlock2()
+	takeDump()
+	for _, tt := range []test{
+		{n: 0, want: "M1> is locked by"},
+		{n: 1, want: "waiting"},
+		{n: 2, want: "waiting"},
+		{n: 3, want: "waiting"},
+		{n: 4, want: "M3> is idle"},
+		{n: 5, want: "ghost waiting"},
+		{n: 6, want: ""},
+	} {
+		testFunc("Dropped M2", tt)
+	}
+
+	// Remove M3's ghose
+	delete(m3.waiting, "ghost")
+	takeDump()
+	for _, tt := range []test{
+		{n: 0, want: "M1> is locked by"},
+		{n: 1, want: "waiting"},
+		{n: 2, want: "waiting"},
+		{n: 3, want: "waiting"},
+		{n: 4, want: ""},
+	} {
+		testFunc("Dropped M3", tt)
+	}
+
+	// Unlock m1 and wait for a goroutine to get the lock.
+	// Take a dump and then tell the goroutine to unlock and wait for it.
+	unlock1()
+	<-locked
+	takeDump()
+	for _, tt := range []test{
+		{n: 0, want: "M1> is locked by"},
+		{n: 1, want: "waiting"},
+		{n: 2, want: "waiting"},
+		{n: 3, want: ""},
+	} {
+		testFunc("1G removed", tt)
+	}
+	if failed {
+		t.Logf("%s", buf.String())
+	}
+
+	// Tell the first goroutine to unlock.
+	// Wait for another goroutine to grab it.
+	goUnlock()
+	<-locked
+	takeDump()
+	for _, tt := range []test{
+		{n: 0, want: "M1> is locked by"},
+		{n: 1, want: "waiting"},
+		{n: 2, want: ""},
+	} {
+		testFunc("2Gs removed", tt)
+	}
+	if failed {
+		t.Logf("%s", buf.String())
+	}
+
+	// Tell the second goroutine to unlock.
+	// Wait for another goroutine to grab it.
+	goUnlock()
+	<-locked
+	takeDump()
+	for _, tt := range []test{
+		{n: 0, want: "M1> is locked by"},
+		{n: 1},
+	} {
+		testFunc("3Gs removed", tt)
+	}
+	if failed {
+		t.Logf("%s", buf.String())
+	}
+
+	// Tell the third goroutine to unlock.
+	// All locks are now idle.
+	goUnlock()
+	takeDump()
+	for _, tt := range []test{
+		{n: 0},
+	} {
+		testFunc("all unlocked", tt)
+	}
+	if failed {
+		t.Logf("%s", buf.String())
+	}
 }
